350d42c51f9ecee1f1a1d731def4be7d642357c0
novo52 <22119302+novo52@users.noreply.github.com>
2023-12-24 17:45:05 -0700

    Genericized quicksort

--- sort.c
+++ sort.c
@@ -2,14 +2,17 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void quicksort(int *arr, int n, bool desc) {
+#include "util.h"
+#include "sort.h"
+
+void quicksort_int(int *arr, int n, bool desc) {
 	if (n <= 1) return;
 	
 	// Partition
 	int i = -1;
 	for (int j = 0; j < n; j++) {
-		if (!desc && arr[j] <= arr[n-1] ||
-		     desc && arr[j] >= arr[n-1]) {
+		if ((!desc && (arr[j] <= arr[n-1])) ||
+		     (desc && (arr[j] >= arr[n-1]))) {
 			i++;
 			int temp = arr[i];
 			arr[i] = arr[j];
@@ -17,6 +20,26 @@ void quicksort(int *arr, int n, bool desc) {
 		}
 	}	
 
-	quicksort(arr, i, desc);
-	quicksort(arr+i+1, n-i-1, desc);
+	quicksort_int(arr, i, desc);
+	quicksort_int(arr+i+1, n-i-1, desc);
 }
+
+
+void quicksort(void *arr, int n, size_t sizeof_element, ComparisonFn comp, bool desc) {
+	if (n <= 1) return;
+
+	// Partition
+	int i = -1;
+	for (int j = 0; j < n; j++) {
+		int c = comp(arr + j*sizeof_element, arr + (n-1)*sizeof_element);
+		if ((!desc && (c <= 0)) ||
+		     (desc && (c >= 0))) {
+			i++;
+			if (i != j) memswap(arr + i*sizeof_element, arr + j*sizeof_element, sizeof_element);
+		}
+	}	
+
+	quicksort(arr, i, sizeof_element, comp, desc);
+	quicksort(arr+(i+1)*sizeof_element, n-i-1, sizeof_element, comp, desc);
+}
+
--- sort.h
+++ sort.h
@@ -3,7 +3,17 @@
 
 #include <stdbool.h>
 
-// Sorts n int elements of arr in ascending order
-void quicksort(int *arr, int n, bool desc);
+// Sorts n int elements of arr in ascending or descending order
+void quicksort_int(int *arr, int n, bool desc);
 
+// A comparison function
+// Return value should be:
+// 	< 0 if elem1 < elem2,
+// 	= 0 if elem1 == elem2,
+// 	> 0 if elem1 > elem2,
+typedef int ComparisonFn(void *elem1, void *elem2);
+
+// Sorts n elements of any type via a custom comparison function
+// in ascending or descending order
+void quicksort(void *arr, int n, size_t sizeof_element, ComparisonFn comp, bool desc);
 #endif
