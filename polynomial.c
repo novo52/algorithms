7f5b9a6207afb9d474892b14a465261128758770
novo52 <22119302+novo52@users.noreply.github.com>
2024-01-08 17:39:11 -0700

    Added function to solve roots of an arbitrary polynomial

--- polynomial.c
+++ polynomial.c
@@ -2,8 +2,46 @@
 #include <stdio.h>
 #include <math.h>
 #include <tgmath.h>
+#include <complex.h>
+#include <stdbool.h>
+#include <limits.h>
 
 #include "polynomial.h"
+
+Polynomial *polynomial_alloc(int degree) {
+	double *p = calloc(degree+1, sizeof(double));
+	Polynomial *poly = malloc(sizeof(Polynomial));
+	poly->d = degree;
+	poly->p = p;
+	return poly;
+}
+
+Polynomial *polynomial_alloc_from_terms(int degree, double *p) {
+	Polynomial *poly = malloc(sizeof(Polynomial));
+	poly->d = degree;
+	poly->p = p;
+	return poly;
+}
+
+void polynomial_free(Polynomial *poly) {
+	free(poly->p);
+	free(poly);
+}
+
+void polynomial_print(Polynomial *polynomial) {
+	for (int d = polynomial->d; d >= 0; d--) {
+		if (polynomial->p[d] >= 0.0) {
+			if (d != polynomial->d) printf(" + ");
+			printf("%.3lf", polynomial->p[d]);
+
+		} else {
+			if (d != polynomial->d) printf(" - ");
+			printf("%.3lf", -polynomial->p[d]);
+		}
+		if (d >= 2) printf("*x^%d", d);
+		if (d == 1) printf("*x");
+	}
+}
 
 double eval_quadratic(double a, double b, double c, double x) {
 	return a*x*x + b*x + c;
@@ -75,8 +113,6 @@ void solve_cubic(double a, double b, double c, double d,
 		double p2 = p1-2.0*M_PI/3.0;
 		double p3 = p1+2.0*M_PI/3.0;
 			
-		printf("p1=%lf, p2=%lf, p3=%lf\n", p1, p2, p3);
-
 		solutions[0] = 2.0 * sqrt_q * cos(p3)-b/3.0;
 		solutions[1] = 2.0 * sqrt_q * cos(p2)-b/3.0;
 		solutions[2] = 2.0 * sqrt_q * cos(p1)-b/3.0;
@@ -98,6 +134,200 @@ void solve_cubic_vertices(double a, double b, double c, double d,
 	solve_quadratic(3.0*a, 2.0*b, c, solutions, solution_count);
 }
 
+// https://en.wikipedia.org/wiki/Geometrical_properties_of_polynomial_roots#Lagrange's_and_Cauchy's_bounds
+double cauchy_roots_upper_bound(Polynomial *poly) {
+	double max_value = 0.0;
+	for (int i = 0; i < poly->d; i++) {
+		double value = abs(poly->p[i]);
+		if (value > max_value) {
+			max_value = value;
+		}
+	}
+
+	return 1.0 + max_value/poly->p[poly->d];
+}
+
+void roots_bound(Polynomial *polynomial, double *upper, double *lower) {
+	// Upper is trivial
+	*upper = cauchy_roots_upper_bound(polynomial);
+	
+	// Lower is symmetric
+	// flip the terms of the polynomial, find the upper root, and take the inverse
+	double *rp = malloc(sizeof(double) * (polynomial->d+1));
+	for (int i = 0; i <= polynomial->d; i++) rp[i] = polynomial->p[polynomial->d-i];
+	double *p = polynomial->p;
+	polynomial->p = rp;
+	*lower = 1.0/cauchy_roots_upper_bound(polynomial);
+	polynomial->p = p;
+	free(rp);
+}
+
+double polynomial_get_term(Polynomial *polynomial, int i) {
+	if (i <= polynomial->d) return polynomial->p[i];
+	return 0.0;
+}
+
+// Trim zeros off the polynomial
+void polynomial_shrink(Polynomial *polynomial) {
+	for (; polynomial->d >= 0; polynomial->d--) if (polynomial->p[polynomial->d] != 0.0) break;
+}
+
+Polynomial *polynomial_derivative(Polynomial *polynomial) {
+	Polynomial *answer = polynomial_alloc(polynomial->d-1);
+	// Apply power rule to every term
+	for (int i = 0; i < polynomial->d; i++) answer->p[i] = (double)(i+1)*polynomial->p[i+1];
+	return answer;
+}
+
+Polynomial *polynomial_sum(Polynomial *p1, Polynomial *p2) {
+	int d = p1->d;
+	if (p2->d > d) d = p2->d;
+	Polynomial *sum = polynomial_alloc(d);
+	for (int i = 0; i <= d; i++) 
+		sum->p[i] = polynomial_get_term(p1, i) + polynomial_get_term(p2, i);
+	polynomial_shrink(sum);
+	return sum;
+}
+
+Polynomial *polynomial_sub(Polynomial *p1, Polynomial *p2) {
+	int d = p1->d;
+	if (p2->d > d) d = p2->d;
+	Polynomial *dif = polynomial_alloc(d);
+	for (int i = 0; i <= d; i++)
+		dif->p[i] = polynomial_get_term(p1, i) - polynomial_get_term(p2, i);
+	polynomial_shrink(dif);
+	return dif;
+}
+
+Polynomial *polynomial_mul(Polynomial *p1, Polynomial *p2) {
+	int d = (p1->d+1)*(p2->d+1)-1;
+	Polynomial *prod = polynomial_alloc(d);
+	for (int a = 0; a <= p1->d; a++) {
+		for (int b = 0; b <= p2->d; b++) {
+			prod->p[a+b] += p1->p[a]*p2->p[b];
+		}
+	}
+	polynomial_shrink(prod);
+	return prod;
+}
+
+bool polynomial_is_zero(Polynomial *polynomial) {
+	return polynomial->d == 0 && polynomial->p[0] == 0.0;
+}
+
+void polynomial_div(Polynomial *num, Polynomial *denom,
+		Polynomial **quot, Polynomial **remainder) {
+
+	// q = 0
+	*quot = polynomial_alloc(0);
+	(*quot)->p[0] = 0.0;
+
+	// r = n
+	*remainder = polynomial_alloc(num->d);
+	for (int i = 0; i <= num->d; i++) (*remainder)->p[i] = num->p[i];
+
+	while(!polynomial_is_zero(*remainder) && (*remainder)->d >= denom->d) {
+		// t = lead(r) / lead(d)
+		int d = (*remainder)->d - denom->d;
+		double val = (*remainder)->p[(*remainder)->d] / denom->p[denom->d];
+		Polynomial *t = polynomial_alloc(d);
+		t->p[d] = val;
+	
+		// Calculate new values
+		Polynomial *new_quot = polynomial_sum(*quot, t);
+		Polynomial *intermediate = polynomial_mul(t, denom);
+		Polynomial *new_remainder = polynomial_sub(*remainder, intermediate);
+
+		// Replace old values
+		polynomial_free(*quot);
+		*quot = new_quot;
+
+		polynomial_free(*remainder);
+		*remainder = new_remainder;
+
+		// Free
+		polynomial_free(t);
+		polynomial_free(intermediate);
+	}
+}
+
+double complex polynomial_eval(Polynomial *polynomial, complex double z) {
+	double complex result = 0.0 + 0.0 * I;
+	for (int i = 0; i <= polynomial->d; i++) {
+		double complex term = polynomial->p[i];
+		for (int j = 0; j < i; j++) {
+			term *= z;
+		}
+		result += term;
+	}
+	return result;
+}
+		
+
+
+
+// todo: use a more numerically stable method
+// file:///home/tyler/Downloads/bini1996.pdf section 3
+//void poly_over_derivative_at_x(Polynomial *polynomial, double x) {
+
+double complex alberth_update(Polynomial *p, Polynomial *d, 
+		double complex *solutions, int k) {
+	double complex z = solutions[k];
+	
+	double complex repulsion = 0.0;
+	for (int j = 0; j < p->d-1; j++) {
+		if (k == j) continue;
+		repulsion += 1.0 / (z - solutions[j]);
+	}
+
+	double complex p_dp_z = 
+			polynomial_eval(p, z) / polynomial_eval(d, z);
+
+	double complex w = p_dp_z / (1 - p_dp_z*repulsion);
+	
+	return w;
+}
+
+
+// https://en.wikipedia.org/wiki/Aberth_method
+void solve_polynomial(Polynomial *polynomial, double complex *solutions) {
+
+	Polynomial *derivative = polynomial_derivative(polynomial);
+	
+	double upper_bound, lower_bound;
+	roots_bound(polynomial, &upper_bound, &lower_bound);
+
+	// Generate d-1 approximation roots equally spaced around the origin
+	// with random magnitudes between the upper and lower bounds
+	rand();
+	for (int i = 0; i < polynomial->d-1; i++) {
+		double t = (double)i/(polynomial->d-1)*2.0*M_PI;
+		double random = (double)rand()/(double)INT_MAX;
+		double r = random*(upper_bound-lower_bound)+lower_bound;
+		double a = r*cos(t);
+		double b = r*sin(t);
+		solutions[i] = a + b*I;
+	}
+
+
+	double complex *offsets = malloc(sizeof(double complex) * polynomial->d);
+
+	for (int i = 0; i < 20; i++) {
+		for (int k = 0; k < polynomial->d-1; k++) {
+			offsets[k] = alberth_update(polynomial, derivative, solutions, k);
+		}
+		for (int k = 0; k < polynomial->d-1; k++) {
+			solutions[k] -= offsets[k];
+		}
+	}
+	
+	free(offsets);
+
+}
+
+
+
+/*
 void solve_polynomial(Polynomial *polynomial, double *solutions, int *solution_count) {
 	if (polynomial->d == 0) {
 		*solution_count = 1;
@@ -113,3 +343,4 @@ void solve_polynomial(Polynomial *polynomial, double *solutions, int *solution_c
 				polynomial->p[0], solutions, solution_count);
 	}
 }
+*/
--- polynomial.h
+++ polynomial.h
@@ -1,10 +1,22 @@
 #ifndef POLYNOMIAL_HEADER
 #define POLYNOMIAL_HEADER
+#include <complex.h>
 
 typedef struct Polynomial {
 	int d;
 	double *p; // p[d+1]
 } Polynomial;
+
+// Allocate and free a polynomial
+Polynomial *polynomial_alloc(int degree);
+void polynomial_free(Polynomial *poly);
+
+// Takes ownership of *p
+// Note: p[0] = dth term
+Polynomial *polynomial_alloc_from_terms(int degree, double *p);
+
+// Print in human readable format
+void polynomial_print(Polynomial *polynomial);
 
 // f(x) = a*x^2 + b*x + c
 double eval_quadratic(double a, double b, double c, double x);
@@ -22,7 +34,7 @@ double eval_cubic(double a, double b, double c, double d, double x);
 
 // solutions of a*x^3 + b*x^2 + c*x + d = 0 for x
 // places 1, 2, or 3 unique solutions in the first solution_count indices of solutions[]
-void solve_cubic(double a_, double a2, double a1, double a0, 
+void solve_cubic(double a, double b, double c, double d, 
 		double solutions[3], int *solution_count);
 
 // for f(x) = a*x^3 + b*x^2 + c*x + d
@@ -30,6 +42,13 @@ void solve_cubic(double a_, double a2, double a1, double a0,
 void solve_cubic_vertices(double a, double b, double c, double d, 
 		double solutions[2], int *solution_count);
 
-void solve_polynomial(Polynomial *polynomial, double *solutions, int *solution_count);
+void solve_polynomial(Polynomial *polynomial, double complex *solutions);
 
+Polynomial *polynomial_derivative(Polynomial *polynomial);
+Polynomial *polynomial_mul(Polynomial *p1, Polynomial *p2);
+void polynomial_div(Polynomial *num, Polynomial *denom,
+		Polynomial **quot, Polynomial **remainder);
+
+// Evaluate the polynomial at z
+double complex polynomial_eval(Polynomial *polynomial, complex double z);
 #endif
